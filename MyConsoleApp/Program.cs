0c34b0a97d177795ac4b6531d116d1831beea3f1
sil <narradoresdigitales@gmail.com>
2026-03-06 21:34:23 -0600

    Initial commit - add all course exercises, cleaned nested repo

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var greeting = "Hello, World!";
+var currentDate = DateTime.Today;
+var christmasDay = new DateTime(currentDate.Year, 12, 25);
+
+Console.WriteLine($" {greeting} ");
+Console.WriteLine($"The current date and time is {currentDate}.");
+
+// Calculate the number of days until Christmas
+if (currentDate >= christmasDay)
+{
+    christmasDay = christmasDay.AddYears(1); 
+}
+
+var daysUntilChristmas = (christmasDay - currentDate).Days;
+Console.WriteLine($"The number of days until Christmas is {daysUntilChristmas}.");

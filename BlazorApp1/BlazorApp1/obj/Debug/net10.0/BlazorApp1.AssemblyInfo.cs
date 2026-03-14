47f809d8cd8ac05d5d569a34cd53346edfc7d4bf
sil <narradoresdigitales@gmail.com>
2026-03-14 11:20:44 -0500

    created first blazor web app

--- BlazorApp1/BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27a93a513af0de145a351c8d93d5af69a0870250")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc389735abdcc9cdac02059307d0b16baf7ab0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

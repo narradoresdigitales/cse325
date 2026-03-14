b37469af548d1b56545d514450789d5da6c6322d
sil <narradoresdigitales@gmail.com>
2026-03-14 13:47:36 -0500

    watched through video 6

--- BlazorApp1/BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cfedae0dfa7b2c73493eb41ab3c27919c0bfc9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9f6079eded4c773cd1f96fa43b6b47fbb55d586")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

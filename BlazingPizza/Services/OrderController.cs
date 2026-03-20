e942cd3151b0b60744c69c9f3f2de29fecf96d98
sil <narradoresdigitales@gmail.com>
2026-03-20 05:36:16 -0500

    completed W03 .NET Blazor Application Tutorial

--- BlazingPizza/Services/OrderController.cs
+++ BlazingPizza/Services/OrderController.cs
@@ -87,4 +87,26 @@ public class OrdersController : Controller
         Console.WriteLine("=== Order processing complete ===");
         return order.OrderId;
     }
+
+    [HttpGet("{orderId}")]
+    public async Task<ActionResult<OrderWithStatus>> GetOrderWithStatus(int orderId)
+    {
+        var order = await _db.Orders
+            .Where(o => o.OrderId == orderId)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Special)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
+            .SingleOrDefaultAsync();
+
+        if (order == null)
+        {
+            return NotFound();
+        }
+
+        return OrderWithStatus.FromOrder(order);
+    }
+
+
+
+
+
 }
